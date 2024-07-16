d69cd080260dcba9f0a4b7ea4402f1e85c4b5422
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-07-16 21:45:56 +0100

    key created

--- app/src/main/java/com/raphaeloliveira/bmicalculator/util/Constants.kt
+++ app/src/main/java/com/raphaeloliveira/bmicalculator/util/Constants.kt
@@ -3,5 +3,7 @@ package com.raphaeloliveira.bmicalculator.util
 class Constants {
     companion object {
         const val KEY_RESULT = "KEY_RESULT"
+        const val KEY_WEIGHT = "KEY_WEIGHT"
+        const val KEY_HEIGHT = "KEY_HEIGHT"
     }
 }
