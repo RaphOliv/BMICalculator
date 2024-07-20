23783315e470193adb89f4a81ef3700fde4cb2a2
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-07-20 10:45:14 +0100

    MVVM viewmodel implemented

--- app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/MainActivity.kt
+++ app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/MainActivity.kt
@@ -2,10 +2,17 @@ package com.raphaeloliveira.bmicalculator.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.raphaeloliveira.bmicalculator.R
 import com.raphaeloliveira.bmicalculator.databinding.ActivityMainBinding
+import com.raphaeloliveira.bmicalculator.domain.BmiCalculator
 import com.raphaeloliveira.bmicalculator.util.Constants
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,29 +23,65 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-            binding.btnCalcule.setOnClickListener {
+            binding.btnHistory.setOnClickListener {
+                val intent = Intent(this, HistoryActivity::class.java)
+                startActivity(intent)
+            }
 
-            val alturaStr = binding.edittextAltura.text.toString()
-            val pesoStr = binding.edittextPeso.text.toString()
+            binding.btnCalculate.setOnClickListener {
 
-            if (alturaStr.isNotEmpty() && pesoStr.isNotEmpty()){
+            val heightStr = binding.etHeight.text.toString()
+            val weightStr = binding.etWeight.text.toString()
 
-                val altura: Float = alturaStr.toFloat()
-                val peso: Float = pesoStr.toFloat()
+            if (heightStr.isNotEmpty() && weightStr.isNotEmpty()){
 
-                val alturaFinal: Float = altura * altura
-                val imc = peso / alturaFinal
+                val height: Float = heightStr.toFloat()
+                val weight: Float = weightStr.toFloat()
 
+                val bmiCalculator = BmiCalculator()
+                val bmi = bmiCalculator.calculateBmi(height, weight)
 
                 val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra(Constants.KEY_RESULT, imc)
+                intent.putExtra(Constants.KEY_RESULT, bmi)
+                intent.putExtra(Constants.KEY_WEIGHT, weightStr)
+                intent.putExtra(Constants.KEY_HEIGHT, heightStr)
                 startActivity(intent)
 
             } else {
-                Snackbar.make(binding.edittextPeso,
-                    "Preencher todos os campos", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.etWeight,
+                    getString(R.string.fill_fields), Snackbar.LENGTH_SHORT).show()
 
             }
         }
+
+        binding.etHeight.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.etHeight.removeTextChangedListener(this)
+
+                try {
+                    val givenString = s.toString().replace(",", "").replace(".", "")
+                    if (givenString.length > 2) {
+                        val longVal: Long = givenString.toLong()
+                        val format = NumberFormat.getInstance(Locale.US) as DecimalFormat
+                        format.applyPattern("0.00")
+                        val formattedHeight = format.format(longVal / 100.00)
+                        binding.etHeight.setText(formattedHeight)
+                        binding.etHeight.setSelection(formattedHeight.length)
+                    }
+                } catch (nfe: NumberFormatException) {
+                    nfe.printStackTrace()
+                }
+
+                binding.etHeight.addTextChangedListener(this)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
     }
 }
