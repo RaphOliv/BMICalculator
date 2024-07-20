f7b78d13b3c00777e69bdff4527190785929315a
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-07-20 10:45:33 +0100

    MVVM viewmodel implemented

--- app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/ResultActivity.kt
+++ app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/ResultActivity.kt
@@ -1,49 +1,57 @@
 package com.raphaeloliveira.bmicalculator.presentation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.raphaeloliveira.bmicalculator.R
+import com.raphaeloliveira.bmicalculator.data.HistoryDao
 import com.raphaeloliveira.bmicalculator.databinding.ActivityResultBinding
+import com.raphaeloliveira.bmicalculator.presentation.viewmodel.ResultViewModel
+import com.raphaeloliveira.bmicalculator.presentation.viewmodel.ResultViewModelFactory
+import com.raphaeloliveira.bmicalculator.domain.usecase.CreateHistoryUseCase
 import com.raphaeloliveira.bmicalculator.util.Constants
+import org.koin.android.ext.android.inject
+import java.util.Locale
 
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultBinding
 
+    private val viewModel: ResultViewModel by viewModels{
+        val createHistoryUseCase = CreateHistoryUseCase(historyDao)
+        ResultViewModelFactory(createHistoryUseCase, resources)
+    }
+
+    private val historyDao: HistoryDao by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityResultBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    val result = intent.getFloatExtra(Constants.KEY_RESULT, 0f)
+        val result = intent.getFloatExtra(Constants.KEY_RESULT, 0.0f)
+        val bmiStr = String.format(Locale.US,"%.2f", result).toFloat()
 
-    binding.tvResult.text = result.toString()
-    binding.bmiIndicatorView.bmiValue(result)
+        binding.tvResult.text = bmiStr.toString()
+        binding.bmiIndicatorView.bmiValue(bmiStr)
 
-    val classificationText = when {
-        result < 18.5f -> getString(R.string.underweight)
-        result in 18.5f..24.9f -> getString(R.string.healthyweight)
-        result in 25f..29.9f -> getString(R.string.overweight)
-        result in 30f..39.9f -> getString(R.string.obesity)
-        else -> getString(R.string.severe_obesity)
-    }
+        binding.btnHistory.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
 
-    val classificationColor = when {
-        result < 18.5f -> R.color.colorUnderWeight
-        result in 18.5f..24.9f -> R.color.colorNormal
-        result in 25f..29.9f -> R.color.colorOverWeight
-        result in 30f..39.9f -> R.color.colorObesity
-        else -> R.color.colorSevereObesity
-    }
+        viewModel.classifyBmi(bmiStr)
+        viewModel.classification.observe(this) { classification: String ->
+            binding.tvClassification.text = classification
 
-    binding.tvClassification.text = classificationText
-    binding.tvClassification.setTextColor(ContextCompat.getColor(this, classificationColor))
-}
+            val weight = intent.getStringExtra(Constants.KEY_WEIGHT).toString()
+            val height = intent.getStringExtra(Constants.KEY_HEIGHT).toString()
+            viewModel.insertBmiDataInDatabase(bmiStr, classification, weight, height)
+        }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-       finish()
-        return super.onOptionsItemSelected(item)
+        viewModel.classificationColor.observe(this) { color: Int ->
+            binding.tvClassification.setTextColor(ContextCompat.getColor(this, color))
+        }
     }
 }
