47764fdfd13ee8608e5ebbc1a5e81c8c5721fb39
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-07-21 18:36:13 +0100

    splash screen updated

--- app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/SplashScreenActivity.kt
+++ app/src/main/java/com/raphaeloliveira/bmicalculator/presentation/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.raphaeloliveira.bmicalculator.presentation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.raphaeloliveira.bmicalculator.R
 import com.raphaeloliveira.bmicalculator.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
